7df2f8abb9370bcbdc92e7a7bd05544712f834d8
Rudraksh Tuwani <rudraksh2709@gmail.com>
2017-10-14 12:30:59 +0530

    Update CAS-To-Pubchem.py

--- CAS-To-Pubchem.py
+++ CAS-To-Pubchem.py
@@ -1,5 +1,5 @@
 def extract_id_type(soup):
-	""" Takes a BS4 object of a puchem page as input and returns the Pubchem ID type and Pubchem ID Value """
+    """ Takes a BS4 object of a puchem page as input and returns the Pubchem ID type and Pubchem ID Value """
     pubhcem_uid_type = soup.find_all('meta', {'name':'pubchem_uid_type'})[0]['content']
     pubhcem_uid_value = soup.find_all('meta', {'name':'pubchem_uid_value'})[0]['content']
     return pubhcem_uid_type +':'+ pubhcem_uid_value
@@ -7,9 +7,9 @@ def extract_id_type(soup):
 
 
 def get_pubchem(cas):
-	""" Extract the mappings to pubchem ids of a given CAS number """
+    """ Extract the mappings to pubchem ids of a given CAS number """
 
-	# Get the search page.
+    # Get the search page.
     url = 'https://www.ncbi.nlm.nih.gov/pccompound?term="{}"'.format(cas)
     r = requests.get(url)
     soup = BeautifulSoup(r.text, "lxml")
